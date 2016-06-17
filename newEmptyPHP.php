3874684702c709ca01288181d59495a998d09f3b
UMayor <hans.neumann@mayor.cl>
2016-06-17 19:18:16 -0400

    primer commit

---
+++ newEmptyPHP.php
@@ -0,0 +1,7 @@
+<!--
+<form name="frmTwitt" method="post" action="sendTwitt.php">
+    <textarea name="twitt" maxlength="500" cols="30" rows="5"></textarea>
+    <input type="submit" value="Enviar Tw ">
+</form>
+-->
+                                
